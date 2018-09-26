cc4b7171dc367619d833a80a8de90066e935051b
Nikki Servatius <nikkiservatius@u.boisestate.edu>
2018-09-26 17:24:41 -0600

    homework 4 check 4

--- phpincludes/Destinationsbody.php
+++ phpincludes/Destinationsbody.php
@@ -1,5 +1,5 @@
 <div class = "bodytext">
-A list of destinations will be here- this data is pulled from a table of user input.
+A list of destinations will be here - this data is pulled from a table of user input.
 
 <?php require_once "maketable.php";
     render_table("destination.txt");
