2173d0b166f01b17ef7939dff3d57f09f70277fb
Nikki Servatius <nikkiservatius@u.boisestate.edu>
2018-09-26 17:13:36 -0600

    homework 4 check 2

--- Vote.php
+++ Vote.php
@@ -1,6 +1,6 @@
 <?php require_once("phpincludes/header.php"); ?>
 <?php require_once("phpincludes/navbar.php"); ?>
 <?php require_once("phpincludes/countdown.php"); ?>
-<?php require_once("phpincludes/VOTEbody.php"); ?>
+<?php require_once("phpincludes/votebody.php"); ?>
 
 <?php require_once("phpincludes/footer.php"); ?>
