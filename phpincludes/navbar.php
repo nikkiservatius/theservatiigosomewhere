08b293f12ec48ee88c417d6cd49f3ba8acf03877
Nikki Servatius <nikkiservatius@u.boisestate.edu>
2018-09-26 17:16:04 -0600

    homework 4 check 3

--- phpincludes/navbar.php
+++ phpincludes/navbar.php
@@ -4,6 +4,6 @@
     <li><a href="Meettheservatii.php">Meet The Servatii</a></li>
     <li><a href="Destinations.php">Destinations</a></li>
     <li><a href="Adddestinations.php">Add Destination</a></li>
-    <li><a href="VOTE.php">VOTE</a></li>
+    <li><a href="Vote.php">VOTE</a></li>
   </ul>
 </div>
