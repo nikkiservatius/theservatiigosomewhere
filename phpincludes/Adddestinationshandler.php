929006555d87f4cd781a560c7f80924ae78030cd
Nikki Servatius <nikkiservatius@u.boisestate.edu>
2018-10-22 17:35:15 -0600

    newtry

---
+++ phpincludes/Adddestinationshandler.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+header("Location: ../index.php");
+
+<?php if (isset($_SESSION['destinations'])) {
+    foreach ($_SESSION['destinations'] as $destinations) {?>
+      <div id = "error">
+        <?php echo $destinations; ?>
+      </div>
+
+      <?php  }
+      unset($_SESSION['destinations']);
+      ?>
+    <?php } ?>
+
+$city = $_POST['city'];
+$state = $_POST['state'];
+$country = $_POST['country'];
+
+$destinations = array();
+
+$bad = false;
+if (strlen($city) == 0) {
+  $_SESSION['destinations'][] = "City is required.";
+  $bad = true;
+}
+if ($country) == United States) {
+  $_SESSION['destinations'][] = "State is required because you choose the United States";
+  $bad = true;
+}
+if (strlen($country) == 0) {
+  $_SESSION['destinations'][] = "Country is required.";
+  $bad = true;
+}
+if ($bad) {
+  header( "Location: ../index.php");
+  $_SESSION['validated'] = 'bad';
+  exit;
+}
+// Got here, means everything validated, and the comment will post.
+
+require_once 'Dao.php';
+$dao = new Dao();
+$dao->saveLogin($city, $state, $country);
+header('Location: ../Destinations.php');
+exit;
+?>
