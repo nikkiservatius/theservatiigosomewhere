87617d4823b827cac465a6f70da9577be1c4663c
Nikki Servatius <nikkiservatius@u.boisestate.edu>
2018-11-07 20:57:28 -0700

    its a new day ugh

--- Homebody.php
+++ Homebody.php
@@ -4,7 +4,8 @@
 if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
 		header('Location: index.php');
 		exit;
-	}?>
+	}
+  ?>
 
 
 
--- Meettheservatiibody.php
+++ Meettheservatiibody.php
@@ -3,7 +3,8 @@
 if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
 		header('Location: index.php');
 		exit;
-	}?>
+	}
+  ?>
 
 <h1>
 
