0d82372b2263c6d2e62aa67288b6a17f928472b6
Nikki Servatius <nikkiservatius@u.boisestate.edu>
2018-11-07 18:57:30 -0700

    its a new day ugh

--- Homebody.php
+++ Homebody.php
@@ -15,12 +15,11 @@
 <div class = "bodytext">
 <br>Welcome!</br>
 <br>
-Our names are Kyle and Nikki Servatius. We are in our late 20's and love to travel. Kyle is close to finishing a travel item on his bucketlist of traveling to every major continent by the time he is 30. He has traveled to many countries in Europe, Thailand, Cambodia, Peru, New Zealand, Mexico and many places domestically. Nikki has lived on both east and west coasts as well as in Tanzania for two and a half years as a Peace Corps Volunteer as an education extension officer. We spent our early years exploring and finally found each other in 2016.</br>
+Thanks for being part of our adventure! We thought we would throw our hats to the wind and let the power of democracy decide where our traveling feet should go.</br>
 <br>
-Kyle is a registered nurse and has his MBA, so he runs the family health clinic at Micron. Nikki is a high school mathematics and computer science teacher at Pathways in Education, an public charter school in Nampa.<br>
+Please feel free to learn more about us, look at the current destinations that can be voted for, add your own destionation that you would like to see us travel to, and of course, go on and VOTE!
 <br>
-We have traveled to Puerto Rico, California, Mexico and to many National Parks together, but our traveling is far from over.</br>
+When the countdown strikes 0, the destination with the most votes is where we will buy our next plane tickets to and pack our bags! </br>
 <br>
-We recently tied the knot (Summer 2018), but since Nikki is working on her Masters degree and an additional teaching endorsement, we have not had an opportunity to go on a honeymoon. That's where you come in.
-</br>
+Thanks for your help and wish us luck! </br>
 </h1>
