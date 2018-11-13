654a216f93f81b60b29d4db6686dd4d034b89f4b
Nikki Servatius <nikkiservatius@u.boisestate.edu>
2018-11-12 19:52:04 -0700

    its a new day ugh

--- Home.php
+++ Home.php
@@ -1,4 +1,4 @@
-
+<?php $thisPage = "Home";?>
 <?php require_once("header.php"); ?>
 <?php require_once("navbar.php"); ?>
 <?php require_once("countdown.php"); ?>
--- Homebody.php
+++ Homebody.php
@@ -1,5 +1,4 @@
-
-<?php $thisPage = "Home";
+<?php
 session_start();
 if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
 		header('Location: index.php');
